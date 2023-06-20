d16919dba7a8a5550d85c90dcdfa5bb15a5b67f7
Etuhu <andgiz@mail.ru>
2023-06-20 17:50:44 +0700

    Полностью завершена адаптивная верстка главной страницы

--- js/front.js
+++ js/front.js
@@ -85,19 +85,20 @@ let offerSliderParams = {
 		  spaceBetween: 10
 		},
 		500: {
-          slidesPerView: 3,
+          slidesPerView: 2.3,
 		  spaceBetween: 10
 		},
 		768: {
-		  slidesPerView: 3.2,
+		  slidesPerView: 3.4,
 		  spaceBetween: 12
 		},
-		1200: {
+		992: {
           slidesPerView: 1,
 		  spaceBetween: 0
 		},
 	}
 }
+
 
 //Инициализируем слайдер галереи на главной странице
 let offerSlider = new Swiper(".offer-content-gallery-swiper", offerSliderParams);
@@ -117,6 +118,22 @@ const resizeHandlerSlider = () => {
     }
 }
 // window.addEventListener('resize', resizeHandlerSlider);
+
+//Изменяем верикальный отступ списка адресов в зависимости от высоты блока с фильтрами по районам
+let addressMapBlock = document.querySelector(".map .offer-list-wrapper");
+let locationsFilter = document.querySelector(".map .locations");
+
+let addressBlockResizer = function () {
+	addressMapBlock.style.paddingTop = (locationsFilter.offsetHeight + 20) + 'px';
+};
+
+if (documentWidth > 991) {
+	addressBlockResizer();
+	window.addEventListener('resize', function () {
+		addressBlockResizer();
+		resizeHandlerSlider();
+	});
+}
 
 //Слайдер Виды носителей на главной
 const aboutSwiper = new Swiper(".about-swiper", {
@@ -149,10 +166,39 @@ let map = document.getElementById("map");
 if (map) {
     DG.then(function () {
         map = DG.map("map", {
-            center: [55.385164, 86.09953],
-            zoom: 16,
+            center: [55.344563, 86.097347],
+            zoom: 15,
+			zoomControl: false,
+			fullscreenControl: false
         });
-        DG.marker([55.385147, 86.098957], {}).addTo(map); /* Координаты маркера и текст попапа */
+		mapicon = DG.icon({
+			iconUrl: 'img/pin-pink.svg',
+			iconAnchor: [25, 70],
+			className: 'map-icon',
+			iconSize: [50, 70] /* Размер иконки */
+		});
+		trIcon = DG.divIcon({
+			className: 'map-icon',
+			html: '<svg width="51" height="71" viewBox="0 0 51 71" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M50.8258 26.1453C50.8326 25.9037 50.836 25.6613 50.836 25.418C50.836 11.38 39.456 0 25.418 0C11.38 0 0 11.38 0 25.418C0 25.66 0.00338205 25.9012 0.0101015 26.1416C0.00338965 26.3054 0 26.4699 0 26.6351C0 32.4287 2.39041 38.0636 8.68515 46.635C18.4858 60.0477 20.7169 64.0159 23.5057 71H27.2506C29.8801 64.4127 32.2705 60.2064 42.1508 46.7144C48.844 37.5874 50.836 32.27 50.836 26.6351C50.836 26.4711 50.8326 26.3079 50.8258 26.1453Z" fill="#F20988"/></svg><img src="img/triangle.png" width="25" class="icon-type">',
+			iconSize: [51, 71] /* Размер иконки */,
+			iconAnchor: [25, 71],
+		});
+		sqrIcon = DG.divIcon({
+			className: 'map-icon',
+			html: '<svg width="51" height="71" viewBox="0 0 51 71" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M50.8258 26.1453C50.8326 25.9037 50.836 25.6613 50.836 25.418C50.836 11.38 39.456 0 25.418 0C11.38 0 0 11.38 0 25.418C0 25.66 0.00338205 25.9012 0.0101015 26.1416C0.00338965 26.3054 0 26.4699 0 26.6351C0 32.4287 2.39041 38.0636 8.68515 46.635C18.4858 60.0477 20.7169 64.0159 23.5057 71H27.2506C29.8801 64.4127 32.2705 60.2064 42.1508 46.7144C48.844 37.5874 50.836 32.27 50.836 26.6351C50.836 26.4711 50.8326 26.3079 50.8258 26.1453Z" fill="#F20988"/></svg><img src="img/sqr.png" width="25" class="icon-type">',
+			iconSize: [51, 71] /* Размер иконки */,
+			iconAnchor: [25, 71],
+		});
+		xIcon = DG.divIcon({
+			className: 'map-icon',
+			html: '<svg width="51" height="71" viewBox="0 0 51 71" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M50.8258 26.1453C50.8326 25.9037 50.836 25.6613 50.836 25.418C50.836 11.38 39.456 0 25.418 0C11.38 0 0 11.38 0 25.418C0 25.66 0.00338205 25.9012 0.0101015 26.1416C0.00338965 26.3054 0 26.4699 0 26.6351C0 32.4287 2.39041 38.0636 8.68515 46.635C18.4858 60.0477 20.7169 64.0159 23.5057 71H27.2506C29.8801 64.4127 32.2705 60.2064 42.1508 46.7144C48.844 37.5874 50.836 32.27 50.836 26.6351C50.836 26.4711 50.8326 26.3079 50.8258 26.1453Z" fill="#F20988"/></svg><img src="img/x.png" width="25" class="icon-type">',
+			iconSize: [51, 71] /* Размер иконки */,
+			iconAnchor: [25, 71],
+		});
+		DG.marker([55.359104, 86.133663], {icon: sqrIcon}).addTo(map);
+		DG.marker([55.344563, 86.097347], {icon: trIcon}).addTo(map);
+		DG.marker([55.338751, 86.062210], {icon: xIcon}).addTo(map);
+        // DG.marker([55.344563, 86.097347], {icon: mapicon}).addTo(map);
     });
 }
 
@@ -178,22 +224,6 @@ function backToTop() {
 
 window.addEventListener("scroll", trackScroll);
 scrollButton.addEventListener("click", backToTop);
-
-//Изменяем верикальный отступ списка адресов в зависимости от высоты блока с фильтрами по районам
-let addressMapBlock = document.querySelector(".map .offer-list-wrapper");
-let locationsFilter = document.querySelector(".map .locations");
-
-console.log(addressMapBlock.style.paddingTop, locationsFilter.offsetHeight);
-
-let addressBlockResizer = function () {
-	addressMapBlock.style.paddingTop = (locationsFilter.offsetHeight + 20) + 'px';
-};
-
-addressBlockResizer();
-window.addEventListener('resize', function () {
-	addressBlockResizer();
-	resizeHandlerSlider();
-});
 
 
 //Подключение библиотеки для маски телефона
